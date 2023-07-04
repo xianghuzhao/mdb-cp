061c3334150ae910bbd812eec4b56d59f6b80fd0
Xianghu Zhao <xianghuzhao@gmail.com>
2023-07-04 17:23:49 +0800

    Try to build

--- .github/workflows/install-rust.sh
+++ .github/workflows/install-rust.sh
@@ -1,28 +1,21 @@
 #!/bin/sh
 
 set -ex
+
 if [ -z "$1" ]
 then
-    echo "First parameter must be toolchain to install."
+    echo "Target must be provided."
     exit 1
 fi
-TOOLCHAIN="$1"
 
-rustup set profile minimal
-rustup component remove --toolchain=$TOOLCHAIN rust-docs || echo "already removed"
-rustup update --no-self-update $TOOLCHAIN
-if [ -n "$2" ]
+target="$1"
+
+HOST=$(rustc -Vv | grep ^host: | sed -e "s/host: //g")
+if [ "$HOST" != "$TARGET" ]
 then
-    TARGET="$2"
-    HOST=$(rustc -Vv | grep ^host: | sed -e "s/host: //g")
-    if [ "$HOST" != "$TARGET" ]
-    then
-        rustup component add llvm-tools-preview --toolchain=$TOOLCHAIN
-        rustup component add rust-std-$TARGET --toolchain=$TOOLCHAIN
-    fi
+		rustup target install "$target"
 fi
 
-rustup default $TOOLCHAIN
 rustup -V
 rustc -Vv
 cargo -V
